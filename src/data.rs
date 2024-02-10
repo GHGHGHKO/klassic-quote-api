f9d762f5ed390ed049ae619c07a26649c47a460f
GHGHGHKO <gudrb963@gmail.com>
2024-02-10 22:45:54 +0900

    feat: add `/quotes` `/random-quote` API

---
+++ src/data.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+
+use tokio::fs;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct QuoteItem {
+    pub quote: String,
+    pub author: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IdentifiableQuoteItem {
+    pub id: usize,
+
+    #[serde(flatten)]
+    pub item: QuoteItem,
+}
+
+impl IdentifiableQuoteItem {
+    pub fn new(id: usize, item: QuoteItem) -> IdentifiableQuoteItem {
+        IdentifiableQuoteItem { id, item }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum QuoteStoreError {
+    #[error("serialization error")]
+    SerializationError(#[from] serde_json::error::Error),
+}
+
+#[derive(Default)]
+pub struct QuoteStore {
+    store: HashMap<usize, IdentifiableQuoteItem>,
+    id_generator: AtomicUsize,
+}
+
+impl QuoteStore {
+
+    pub fn get_quotes(&self, pagination: Pagination) -> Vec<IdentifiableQuoteItem> {
+        self.store
+            .values()
+            .skip(pagination.offset.unwrap_or(0))
+            .take(pagination.limit.unwrap_or(usize::MAX))
+            .cloned()
+            .collect::<Vec<_>>()
+    }
+
+    pub fn get_random_quote(&self, pagination: Pagination) -> Option<&IdentifiableQuoteItem> {
+        let min_offset: usize = 1;
+        let max_offset: usize = self.get_quotes(pagination).len() - 1;
+
+        let random_offset: usize = rand::thread_rng()
+            .gen_range(min_offset..=max_offset);
+        self.store.get(&random_offset)
+    }
+
+    pub async fn add_quotes(&mut self) {
+        let quotes_file = fs::read_to_string("./the_war_of_flower.json")
+            .await
+            .unwrap();
+        let quotes: Vec<QuoteItem> = serde_json::from_str(&quotes_file).unwrap();
+        for quote in quotes {
+            let id = self.id_generator.fetch_add(1, Ordering::Relaxed);
+            let new_item = IdentifiableQuoteItem::new(id, quote);
+            self.store.insert(id, new_item.clone());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+mod data;
+
+use std::net::SocketAddr;
+use std::sync::Arc;
+use axum::extract::{Query, State};
+use axum::{Json, Router};
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use tokio::sync::RwLock;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use data::QuoteStore;
+use crate::data::{Pagination};
+
+type Db = Arc<RwLock<QuoteStore>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "quote_axum=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // QuoteStore::get_random_quote(&Default::default(), 0);
+    let db = Db::default();
+    db.write().await.add_quotes().await;
+
+    let app = Router::new()
+        .nest("/v1", Router::new()
+            .route("/quotes", get(get_quotes))
+            .route("/random-quote", get(get_random_quote)),
+        )
+        .with_state(db)
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn get_quotes(pagination: Option<Query<Pagination>>, State(db): State<Db>) -> impl IntoResponse {
+    let quotes = db.read().await;
+    let Query(pagination) = pagination.unwrap_or_default();
+    Json(quotes.get_quotes(pagination))
+}
+
+async fn get_random_quote(pagination: Option<Query<Pagination>>,
+                          State(db): State<Db>)
+    -> impl IntoResponse {
+    let quote = db.read().await;
+    let Query(pagination) = pagination.unwrap_or_default();
+    if let Some(item) = quote.get_random_quote(pagination) {
+        Json(item).into_response()
+    } else {
+        (StatusCode::NOT_FOUND, "Not found").into_response()
+    }
 }
