fb53f9d25b1048516700de62133e338c9b2779cb
GHGHGHKO <gudrb963@gmail.com>
2024-04-10 17:19:31 +0900

    feat: add wish quotes

--- src/movie_enum.rs
+++ src/movie_enum.rs
@@ -3,6 +3,7 @@ pub fn str_to_movie(name: String) -> Result<&'static str, &'static str> {
         "new-world" => "신세계",
         "the-war-of-flower" => "타짜",
         "nameless-gangster" => "범죄와의 전쟁",
+        "wish" => "바람",
         _ => return Err("404 Not Found: Unknown movie title"),
     })
 }
